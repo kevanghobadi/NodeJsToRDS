3000440a933a52ac1ddcc7f3a9f504ea16b97b02
Kevan Ghobadi <kevang@gmail.com>
2018-03-20 14:28:50 -0500

    one more test to check the message and a parameter in

--- test/api.test.js
+++ test/api.test.js
@@ -17,12 +17,13 @@ describe('get 200 from /api', () => {
             });
       })
 
-    // it('should call the api with a number parameter and see it in the response',
-    //  (done) => {
-    //   request(app)
-    //     .get('/api?number=100')
-    //     .expect(200)
-    //     .expect(res).to.be.json
-    //     .end(done)
-    //     })
+      it('should call the api', (done) => {
+          chai.request(app)
+          .get('/api?number=100')
+          .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.be.eql('Your number is:100');
+                    done();
+                });
+          })
     })
